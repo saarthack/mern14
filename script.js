b8f17130d8c29bce5ca73685f4c9646f2848351c
saarthack <ssarthak2003@outlook.com>
2025-04-07 16:32:00 +0530

    set timeout

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-// DOM - Document Object Model 
-// Frontend ki JavaScript 
-
-// 4 pillars of DOM
-
-// 1. Selection of an Element ✅
-// 2. Changing HTML ✅
-// 3. Changing CSS ✅
-// 4. Event Listener ✅
-
-
-var h5 = document.querySelector('h5')
 var btn = document.querySelector('button')
+var h5 = document.querySelector('h5')
 
-var flag = 0
-
-btn.addEventListener('click', function () {
-
-    if(flag == 0){
+btn.addEventListener('click',function(){
+    
+    h5.innerHTML = 'Sending...'
+    h5.style.color = 'gold'
+    btn.innerHTML = 'Adding...'
+    setTimeout(function(){
         h5.innerHTML = 'Friends'
         h5.style.color = 'green'
-        btn.innerHTML = 'Remove Friend'
-        flag = 1
-    }else{
-        h5.innerHTML = 'Stranger'
-        h5.style.color = 'red'
-        btn.innerHTML = 'Add Friend'
-        flag = 0
-    }
-
+        btn.innerHTML = 'Remove'
+    },3000)
 })
