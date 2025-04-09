6618bd0da3f020d5d77eaa76ea13b6d70287935b
saarthack <ssarthak2003@outlook.com>
2025-04-09 16:33:56 +0530

    create element

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 var btn = document.querySelector('button')
-var percent = document.querySelector('#percent')
-var growth = document.querySelector('#growth')
-
-var num = 0
+var body = document.querySelector('body')
 
 btn.addEventListener('click',function(){
-    var int = setInterval(function(){
-        num++
-        percent.innerHTML = num+'%'
-        growth.style.width = num+'%'
-    },40)
 
-    setTimeout(function(){
-        clearInterval(int)
-    },4000)
+    var x = Math.random()*90
+    var y = Math.random()*90
+    var rot = Math.random()*360
+
+    var img = document.createElement('img')
+
+    img.src = 'https://www.freeiconspng.com/thumbs/cartoon-png/spongebob-cartoon-png-32.png'
+    img.style.left = x+'%'
+    img.style.top = y+'%'
+    img.style.rotate = rot+'deg'
+    
+    body.appendChild(img)
+    
 })
+
