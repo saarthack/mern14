5aa9939e9332d0dc924d5497ca1fce355b74c6c2
saarthack <ssarthak2003@outlook.com>
2025-04-18 16:43:53 +0530

    json feature

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-var btn = document.querySelector('button')
-var body = document.querySelector('body')
+// 1. make a whatsapp chat UI with different details of each chat 
 
-btn.addEventListener('click',function(){
+// 2. make a instagram post feature with different data 
 
-    var x = Math.random()*90
-    var y = Math.random()*90
-    var rot = Math.random()*360
+// 3. complete todays task with different data 
 
-    var img = document.createElement('img')
+var arr = [
+    {
+        username:'spider man',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqNbbAG6WN0nfRFZrkN_y12zm9a9b3Apxe9w&s',
+        age:24,
+        superpower:'Web developer',
+        realName:'Peter Parker'
 
-    img.src = 'https://www.freeiconspng.com/thumbs/cartoon-png/spongebob-cartoon-png-32.png'
-    img.style.left = x+'%'
-    img.style.top = y+'%'
-    img.style.rotate = rot+'deg'
-    
-    body.appendChild(img)
-    
+    },
+    {
+        username:'iron man',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUbnZFRhuUP_TPos-xReniz08khAV-rtwY6A&s',
+        age:44,
+        superpower:'AI Engineer',
+        realName:'Tony Stark'
+    },
+    {
+        username:'hulk',
+        image:'https://cosmicbook.news/wp-content/uploads/2025/01/marvel-world-war-hulk-rumors.webp',
+        age:34,
+        superpower:'mechanical engineer',
+        realName:'Bruce Banner'
+    },
+    {
+        username:'Shaktimaan',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIFgfwv0E2Xnxeln3koOxugFhUzs_kRCFx2w&s',
+        age:69,
+        superpower:'gol gol ghumna',
+        realName:'Gangadhar'
+    }
+]
+
+var sum = ''
+
+arr.forEach(function(elem){
+    sum = sum + `<div class="card">
+    <img src="${elem.image}" alt="">
+    <h1>${elem.username}, ${elem.age}</h1>
+    <h2>${elem.realName}</h2>
+    <h5>${elem.superpower}</h5>
+</div>`
 })
 
+
+var main = document.querySelector("#main")
+
+main.innerHTML = sum
